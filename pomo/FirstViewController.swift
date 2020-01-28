53acf0957ce8cb4d8a4bc9c6c0a702d3fbbf04ff
Manya Egon <egon.manya@gmail.com>
2020-01-28 16:58:33 +0100

    Initial Commit

---
+++ pomo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  pomo
+//
+//  Created by Egon Manya on 28.01.20.
+//  Copyright © 2020 Egon Manya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pomo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  pomo
+//
+//  Created by Egon Manya on 28.01.20.
+//  Copyright © 2020 Egon Manya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
