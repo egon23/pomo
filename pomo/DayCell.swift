5003d01efe13af06f4f1b2ba59d80f2ede6805b9
Roland Iana <roland.iana@students.fh-hagenberg.at>
2020-01-29 14:46:53 +0100

    Days TableView and Day DetailView implemented

---
+++ pomo/DayCell.swift
@@ -0,0 +1,21 @@
+//
+//  DayCell.swift
+//  pomo
+//
+//  Created by roli on 29.01.20.
+//  Copyright © 2020 Egon Manya. All rights reserved.
+//
+
+import UIKit
+
+class DayCell: UITableViewCell{
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var tasksLabel: UILabel!
+    @IBOutlet weak var activeLabel: UILabel!
+    @IBOutlet weak var breakLabel: UILabel!
+    @IBOutlet weak var sessionLabel: UILabel!
+    
+    
+    
+}
--- pomo/FirstViewController.swift
+++ pomo/FirstViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var daysTableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        daysTableView.delegate = self
+        daysTableView.dataSource = self
+        
+        
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath) as! DayCell
+        
+        cell.dayLabel.text = "01.01.10"
+        cell.activeLabel.text = "4h 20min"
+        cell.breakLabel.text = "96 min"
+        cell.tasksLabel.text = "Chillin"
+        cell.sessionLabel.text = "1"
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDay" {
+            let vc = segue.destination as! DayViewController
+                if let cell = sender as? DayCell {
+                    vc.date = cell.dayLabel.text!
+                    vc.activeTime = cell.activeLabel.text!
+                    vc.tasks = cell.tasksLabel.text!
+                    vc.breakTime = cell.breakLabel.text!
+                    
+                }
+            
+        }
+
+    }
 }
 
