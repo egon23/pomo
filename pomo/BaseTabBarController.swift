d64ace335e6dee7dee2075b0d983c43d24665788
Manya Egon <egon.manya@gmail.com>
2020-02-02 19:52:56 +0100

    tabbar Animation

--- pomo/BaseTabBarController.swift
+++ pomo/BaseTabBarController.swift
@@ -9,15 +9,44 @@
 import Foundation
 import UIKit
 
-class BaseTabBarController: UITabBarController {
+class BaseTabBarController: UITabBarController, UITabBarControllerDelegate {
 
     @IBInspectable var defaultIndex: Int = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark
-
+        delegate = self
         selectedIndex = defaultIndex
     }
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        if let fromView = tabBarController.selectedViewController?.view,
+            let toView = viewController.view, fromView != toView,
+            let controllerIndex = self.viewControllers?.firstIndex(of: viewController) {
+            
+            let viewSize = fromView.frame
+            let scrollRight = controllerIndex > tabBarController.selectedIndex
+            
+            // Avoid UI issues when switching tabs fast
+            if fromView.superview?.subviews.contains(toView) == true { return false }
+            
+            fromView.superview?.addSubview(toView)
+            
+            let screenWidth = UIScreen.main.bounds.size.width
+            toView.frame = CGRect(x: (scrollRight ? screenWidth : -screenWidth), y: viewSize.origin.y, width: screenWidth, height: viewSize.size.height)
 
+            UIView.animate(withDuration: 0.25, delay: TimeInterval(0.0), options: [.curveEaseOut, .preferredFramesPerSecond60], animations: {
+                fromView.frame = CGRect(x: (scrollRight ? -screenWidth : screenWidth), y: viewSize.origin.y, width: screenWidth, height: viewSize.size.height)
+                toView.frame = CGRect(x: 0, y: viewSize.origin.y, width: screenWidth, height: viewSize.size.height)
+            }, completion: { finished in
+                if finished {
+                    fromView.removeFromSuperview()
+                    tabBarController.selectedIndex = controllerIndex
+                }
+            })
+            return true
+        }
+        return false
+    }
 }
